165141bb6569aab47fe3baf08edc8ae70e8d8d4f
Miguel Abaroa <mabaroa@gmail.com>
2014-02-10 21:55:54 -0500

    Created Post resource and updated index and show views

--- Rakefile
+++ Rakefile
@@ -4,4 +4,4 @@
 
 require File.expand_path('../config/application', __FILE__)
 
-FirstApp::Application.load_tasks
+Bloccit::Application.load_tasks
--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-FirstApp::Application.config.secret_token = 'a285641571d7b1c5623e3a2f3e4dfe38549d455cdcf4838546cc29bafcf0c6851500f5b0310e1856122a36edf3be567215506d1876d6805cb6709870a7f11bd2'
+Bloccit::Application.config.secret_token = 'a285641571d7b1c5623e3a2f3e4dfe38549d455cdcf4838546cc29bafcf0c6851500f5b0310e1856122a36edf3be567215506d1876d6805cb6709870a7f11bd2'
